96bf0338b70e328b072dea7b4ba00aa6e7f7e229
JongGurl <gojkhan13@gmail.com>
2020-08-10 17:55:19 +0900

    miniProjectJava 첫번째 커밋

---
+++ MiniProjectJava/src/kr/co/softcampus/beans/UserBean.java
@@ -0,0 +1,36 @@
+package kr.co.softcampus.beans;
+
+public class UserBean {
+
+	private int user_idx; 
+	private String user_;
+	private String user_id; 
+	private String user_pw;
+	
+	public int getUser_idx() {
+		return user_idx;
+	}
+	public void setUser_idx(int user_idx) {
+		this.user_idx = user_idx;
+	}
+	public String getUser_() {
+		return user_;
+	}
+	public void setUser_(String user_) {
+		this.user_ = user_;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pw() {
+		return user_pw;
+	}
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	} 
+	
+	
+}
