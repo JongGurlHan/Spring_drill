c874f80b0675770ede04cde22fd0c4ebefab3041
JGHAN <JGHAN@DESKTOP-JRJKTRM>
2020-08-10 23:23:04 +0900

    interceptorjava 2nd

--- InterceptorJava/src/kr/co/softcampus/config/ServletAppContext.java
+++ InterceptorJava/src/kr/co/softcampus/config/ServletAppContext.java
@@ -3,9 +3,20 @@ package kr.co.softcampus.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.co.softcampus.interceptor.TestInterceptor1;
+import kr.co.softcampus.interceptor.TestInterceptor2;
+import kr.co.softcampus.interceptor.TestInterceptor3;
+import kr.co.softcampus.interceptor.TestInterceptor4;
+import kr.co.softcampus.interceptor.TestInterceptor5;
+import kr.co.softcampus.interceptor.TestInterceptor6;
+import kr.co.softcampus.interceptor.TestInterceptor7;
+import kr.co.softcampus.interceptor.TestInterceptor8;
 
 // Spring MVC 프로젝트에 관련된 설정을 하는 클래스
 @Configuration
@@ -28,6 +39,47 @@ public class ServletAppContext implements WebMvcConfigurer{
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+	
+	//인터셉터를 등록한다.
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		WebMvcConfigurer.super.addInterceptors(registry);
+		
+		TestInterceptor1 inter1 = new TestInterceptor1();
+		TestInterceptor2 inter2 = new TestInterceptor2();
+		TestInterceptor3 inter3 = new TestInterceptor3();
+		TestInterceptor4 inter4 = new TestInterceptor4();
+		TestInterceptor5 inter5 = new TestInterceptor5();
+		TestInterceptor6 inter6 = new TestInterceptor6();
+		TestInterceptor7 inter7 = new TestInterceptor7();
+		TestInterceptor8 inter8 = new TestInterceptor8();
+		
+		InterceptorRegistration reg1 = registry.addInterceptor(inter1);
+		InterceptorRegistration reg2 = registry.addInterceptor(inter2);
+		InterceptorRegistration reg3 = registry.addInterceptor(inter3);
+		InterceptorRegistration reg4 = registry.addInterceptor(inter4);
+		InterceptorRegistration reg5 = registry.addInterceptor(inter5);
+		InterceptorRegistration reg6 = registry.addInterceptor(inter6);
+		InterceptorRegistration reg7 = registry.addInterceptor(inter7);
+		InterceptorRegistration reg8 = registry.addInterceptor(inter8);
+		
+		//test1으로 요청하게 되면  컨드롤러로 바로 가는게 아니라 inter1인터셉터를 호출해라
+		reg1.addPathPatterns("/test1");
+		reg2.addPathPatterns("/test1");
+		reg3.addPathPatterns("/test2");
+ 
+//		reg4.addPathPatterns("/test1");
+//		reg4.addPathPatterns("/test2");
+		
+		reg4.addPathPatterns("/test1", "/test2");
+		reg5.addPathPatterns("/sub1/test3", "/sub1/test4");
+
+		reg6.addPathPatterns("/*");
+		reg7.addPathPatterns("/sub1/*");
+		
+		reg8.addPathPatterns("/**");
+		reg8.addPathPatterns("/*");
 	}
 }
 
--- InterceptorJava/src/kr/co/softcampus/controller/TestController.java
+++ InterceptorJava/src/kr/co/softcampus/controller/TestController.java
@@ -8,6 +8,23 @@ public class TestController {
 	
 	@GetMapping("/test1")
 	public String test1() {
+		System.out.println("test1");
 		return "test1";
+	}
+	@GetMapping("/test2")
+	public String test2() {
+		System.out.println("test2");
+		return "test2";
+	}
+
+	@GetMapping("/sub1/test3")
+	public String test3() {
+		System.out.println("sub1/test3");
+		return "test3";
+	}
+	@GetMapping("/sub1/test4")
+	public String test4() {
+		System.out.println("sub1/test4");
+		return "test4";
 	}
 }
