95c75e8345de9f511211c274e20782c20437b44a
JongGurl <gojkhan13@gmail.com>
2020-08-11 10:41:38 +0900

    2nd InterceptorXML upload

---
+++ InterceptorXML/src/kr/co/softcampus/interceptor/TestInterceptor4.java
@@ -0,0 +1,37 @@
+package kr.co.softcampus.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class TestInterceptor4 extends HandlerInterceptorAdapter{
+
+	// Controller의 메서드가 호출되기전에 호출됨.
+		//false를 반환하면 요청 처리에 대한 진행이 중단된다. 
+		@Override
+		public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+				throws Exception {
+			System.out.println("TestIncetceptor4 - preHandle");
+			return true;
+		}
+		
+		// Controller의 메서드가 호출되고 난 후  호출됨.
+		@Override
+		public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+				ModelAndView modelAndView) throws Exception {
+			System.out.println("TestIncetceptor4 - postHandle");
+		}
+		
+		// view처리까지 완료되면  호출됨.
+		@Override
+		public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+				throws Exception {
+			 System.out.println("TestIncetceptor4 - afterCompletion");
+		}
+	
+	
+	
+}
---
+++ InterceptorXML/src/kr/co/softcampus/interceptor/TestInterceptor6.java
@@ -0,0 +1,35 @@
+package kr.co.softcampus.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TestInterceptor6 implements HandlerInterceptor{
+
+	// Controller의 메서드가 호출되기전에 호출됨.
+	//false를 반환하면 요청 처리에 대한 진행이 중단된다. 
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("TestIncetceptor6 - preHandle");
+		return true;
+	}
+	
+	// Controller의 메서드가 호출되고 난 후  호출됨.
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("TestIncetceptor6 - postHandle");
+	}
+	
+	// view처리까지 완료되면  호출됨.
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		 System.out.println("TestIncetceptor6 - afterCompletion");
+	}
+	
+	
+}
