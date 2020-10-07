6886bb0ef5768b852e1c532677c7437de6902397
JG HAN <60098769+JongGurlHan@users.noreply.github.com>
2020-10-07 23:34:30 +0900

    10.07 페이징처리

--- Miniproject_java_prac/src/kr/co/softcampus/controller/MainController.java
+++ Miniproject_java_prac/src/kr/co/softcampus/controller/MainController.java
@@ -1,13 +1,29 @@
 package kr.co.softcampus.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.softcampus.beans.ContentBean;
+import kr.co.softcampus.service.MainService;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private MainService mainService;
+	
 	@GetMapping("/main")
 	public String main() {
+		
+		ArrayList<List<ContentBean>> list = new ArrayList<List<ContentBean>>();
+		List<ContentBean>list1 = mainService.getMainList(1);
+		
+		
+		
 		return "main";
 	}
 }
--- Miniproject_java_prac/src/kr/co/softcampus/mapper/BoardMapper.java
+++ Miniproject_java_prac/src/kr/co/softcampus/mapper/BoardMapper.java
@@ -2,10 +2,12 @@ package kr.co.softcampus.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.session.RowBounds;
 
 import kr.co.softcampus.beans.ContentBean;
 
@@ -30,7 +32,7 @@ public interface BoardMapper {
 			"where a1.content_writer_idx = a2.user_idx " + 
 			"      and a1.content_board_idx = #{board_info_idx} " + 
 			"order by a1.content_idx desc")
-	List<ContentBean> getContentList(int board_info_idx);
+	List<ContentBean> getContentList(int board_info_idx, RowBounds rowBounds);
 	
 	@Select("select a2.user_name as content_writer_name, " + 
 			"       to_char(a1.content_date, 'YYYY-MM-DD') as content_date, " + 
@@ -45,6 +47,12 @@ public interface BoardMapper {
 			"content_file = #{content_file, jdbcType=VARCHAR} " +
 			"where content_idx = #{content_idx}")
 	void modifyContentInfo(ContentBean modifyContentBean);
+	
+	@Delete("delete from content_table where content_idx = #{content_idx}")
+	void deleteContentInfo(int content_idx);
+	
+	@Select("select count(*) from content_table where content_board_idx = #{content_board_idx}")
+	int getContentCnt(int content_board_idx);
 } 
 
 
